c1bc71c24db2e519382c6834077565f61eb843bc
Tanner Vanlangeveld <t.vanlangeveld@outlook.com>
2023-01-22 14:23:19 -0700

    Arrays

--- arrays.js
+++ arrays.js
@@ -5,10 +5,18 @@ const faveColors = ['red', 'green', 'black']
 
 //CODE HERE
 
+// let colorCopy = faveColors.slice(0,3)
+// console.log(colorCopy)
+
+const colorCopy = ['red','green','black']
+
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
 //CODE HERE
+
+// colorCopy.push('white')
+// console.log(colorCopy)
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -20,6 +28,8 @@ const numbers = [1, 2, 3, 4, 5]
 
 //CODE HERE
 
+const middleNums = numbers.splice(1,3)
+console.log(middleNums)
 
 //////////////////PROBLEM 4////////////////////
 /* 
@@ -34,3 +44,6 @@ const numbers = [1, 2, 3, 4, 5]
 const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
 
 // CODE HERE
+
+let answers = [];
+for (let i = 0; i < bigOrSmallArray; i++) 
